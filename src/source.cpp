518182cfe5ddb038006a681260eb037bbdee947e
xuchang <870819621@qq.com>
2021-09-18 23:17:48 +0800

    去除多余文件

---
+++ src/source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<queue>
+#define STR "hello"
+int main(){
+
+	char str[6] = "hello!";
+	
+	cout << STR << endl;
+	
+	system("pause");
+	
+
+
+}
